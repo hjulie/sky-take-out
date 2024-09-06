61d3b4ff161dd96ad030a5821383230896fc6dfa
Admin <1749328798@qq.com>
2024-09-06 11:26:38 +0800

    商品浏览业务功能代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.sky.mapper;
+
+import com.sky.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface UserMapper {
+
+    //根据openid查询用户
+    @Select("select * from sky_take_out.user where openid = #{openid}")
+    User getByUserId(String openid);
+
+    //插入数据
+    void insert(User user);
+}
