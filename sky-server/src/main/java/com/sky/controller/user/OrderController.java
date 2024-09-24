bee44c161b5c72dfaf3a8372bf21d611db2324a0
Admin <1749328798@qq.com>
2024-09-24 10:17:12 +0800

    客户催单业务代码开发

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -84,6 +84,14 @@ public class OrderController {
         return Result.success();
     }
 
+    //客户催单
+    @GetMapping("/reminder/{id}")
+    @ApiOperation("客户催单")
+    public Result reminder(@PathVariable("id") Long id){
+        orderService.reminder(id);
+        return Result.success();
+    }
+
 }
 
 
